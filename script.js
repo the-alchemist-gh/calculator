fc5ce65ecb3f59522a26c7d96ff81f121b69b00b
Abdul Malik Jabel Yusif <malikbanks01@gmail.com>
2022-01-31 12:43:30 +0000

    Finished basic calculations

--- script.js
+++ script.js
@@ -7,29 +7,57 @@ class Calculate {
 
   // clear the previous text and current text
   clear(){
-    this.currentText = ''
-    this.previousText = ''
+    this.currentTextInput = ''
+    this.previousTextInput = ''
     this.operator = undefined
   }
   
   // Append new clicked number
   appendNumber(number){
-  
+    if(number=== '.' && this.currentTextInput.includes('.')) return
+    
+    this.currentTextInput = this.currentTextInput.toString() + number.toString();
   }
   
   // choose operator
   clickedOperator(operator){
-  
+    console.log(operator);
+    if(this.previousTextInput !== ''){
+      this.compute()
+    }
+    this.operator = operator;
+    this.previousTextInput = this.currentTextInput;
+    this.currentTextInput = '';
+    // this.compute()
   }
   
   // Calculate everything
   compute(){
-  
+    let result
+    const oldNumber = parseFloat(this.previousTextInput)
+    const newNumber = parseFloat(this.currentTextInput)
+    if(isNaN(oldNumber) || isNaN(newNumber)) return
+    if(this.operator=== 'addition'){
+      result = oldNumber + newNumber
+    } else if(this.operator=== 'subtract'){
+      result = oldNumber - newNumber
+    } else if(this.operator=== 'divide'){
+      result = oldNumber / newNumber
+    } else if(this.operator=== 'multiply'){
+      result = oldNumber * newNumber
+    }
+    console.log(result);
+    this.currentTextInput = result
+    this.operator = undefined
+    this.previousTextInput = ''
+    // return result;
   }
   
   // Display figure
   updateDisplay(){
-    this.currentText.innerText= this.currentTextInput
+    this.currentText.innerText = this.currentTextInput
+    this.previousText.innerText = this.previousTextInput
+
   }
 
 
@@ -52,4 +80,20 @@ for (const button of numberBtn){
   })
 }
 
+for (const operator of operatorBtn){
+  operator.addEventListener('click',()=>{
+    console.log(operator.id)
+    calculator.clickedOperator(operator.id)
+    calculator.updateDisplay()
+  })
+}
 
+equalBtn.addEventListener('click', ()=>{
+  calculator.compute();
+  calculator.updateDisplay();
+})
+
+clearBtn.addEventListener('click', ()=>{
+  calculator.clear();
+  calculator.updateDisplay();
+})
